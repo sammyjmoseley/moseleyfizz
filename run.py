246eb1e8ac1f7ad8c9ac1a6a421002e4aa86510b
Samuel Moseley <samuelmoseley@Samuels-MacBook-Pro-2.local>
2016-11-08 20:11:30 -0500

    adding keypad interaction

--- run.py
+++ run.py
@@ -4,12 +4,25 @@ import twilio.twiml
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
+def hello():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+    
+    with resp.gather(numDigits=1, action=url_for('menu'), method="POST") as g:
+        g.play(url="http://howtodocs.s3.amazonaws.com/et-phone.mp3", loop=3)
 
-    return str(resp)
+    return twiml(resp)
+
+@app.route('/fizz', methods=['POST'])
+def fizz():
+    selected_option = request.form['Digits']
+    
+    resp.say("you entered " + selected_option);
+
+    return twiml(resp)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
